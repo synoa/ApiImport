90e6fb73ff367027db201991025df91cd938a91f
Gerhard Ebelshäuser <g.ebelshaeuser@synoa.de>
2017-09-05 14:53:06 +0200

    Fix for EE Edition

--- code/Model/Image.php
+++ code/Model/Image.php
@@ -7,10 +7,13 @@ class Danslo_ApiImport_Model_Image extends Varien_Image
      */
     public function __destruct()
     {
-        $adpater = $this->_getAdapter();
+        $adapter = $this->_getAdapter();
 
-        if ($adpater instanceof Varien_Image_Adapter_Gd2) {
-            $adpater->destruct();
+        if ($adapter instanceof Varien_Image_Adapter_Gd2) {
+            if (method_exists($adapter, 'destruct')) {
+                $adapter->destruct();
+            }
         }
     }
 }
+
